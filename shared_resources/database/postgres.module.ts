a5f9e42d13dfdd0744421c1ad26fedf5f1aa2c91
ducviet5138 <ducviet5138@gmail.com>
2024-11-30 11:00:24 +0700

    chore(apsp-28): init database and migration

---
+++ shared_resources/database/postgres.module.ts
@@ -0,0 +1,25 @@
+import * as allEntities from "./entities";
+import { Module } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+const entities = (Object.keys(allEntities) as Array<keyof typeof allEntities>).map((key) => allEntities[key]);
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        type: "postgres",
+        host: configService.get("POSTGRES_HOST"),
+        port: configService.get("POSTGRES_PORT"),
+        username: configService.get("POSTGRES_USER"),
+        password: configService.get("POSTGRES_PASSWORD"),
+        database: configService.get("POSTGRES_DB"),
+        entities,
+        logging: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule {}
